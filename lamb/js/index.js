3d7bfbff3f163bb20373bda1337c2f692cb0ee09
GuYun-D <1946306647@qq.com>
2021-06-20 11:04:22 +0800

    绘制侧边栏

---
+++ lamb/js/index.js
@@ -0,0 +1,20 @@
+var menu = getId('menu')
+var aside = getId('aside')
+var info = aside.children[0].children[0]
+var asideBoolean = false
+
+menu.addEventListener('click', function () {
+  if (asideBoolean === false) {
+    document.body.style.overflow = 'hidden'
+    this.style.right = '12.875rem'
+    aside.style.width = '80%'
+    info.style.display = 'flex'
+    asideBoolean = true
+  }else {
+    this.style.right = '.875rem'
+    aside.style.width = '0'
+    info.style.display = 'none'
+    asideBoolean = false
+  }
+
+})
