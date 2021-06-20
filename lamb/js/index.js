21664d14ee730d8ac7c94fc7c26b853e4c835f0a
GuYun-D <1946306647@qq.com>
2021-06-20 17:12:51 +0800

    修改尺寸

--- lamb/js/index.js
+++ lamb/js/index.js
@@ -3,14 +3,18 @@ var aside = getId('aside')
 var info = aside.children[0].children[0]
 var asideBoolean = false
 
+var foot = document.getElementsByTagName('footer')[0]
+
 menu.addEventListener('click', function () {
   if (asideBoolean === false) {
     document.body.style.overflow = 'hidden'
+    foot.style.display = 'none'
     this.style.right = '12.875rem'
     aside.style.width = '80%'
     info.style.display = 'flex'
     asideBoolean = true
   }else {
+    foot.style.display = 'block'
     this.style.right = '.875rem'
     aside.style.width = '0'
     info.style.display = 'none'
